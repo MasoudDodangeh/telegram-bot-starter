2a88bead0c9032b0192e1f4e32c910d76f30f1c0
MasoudDodangeh <dodangeh.masoud@gmail.com>
2025-05-03 09:45:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 
 load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+PORT = int(os.environ.get('PORT', '8443'))
+WEBHOOK_URL = "https://telegram-bot-starter-pqp2.onrender.com"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! من ربات شما هستم.")
+    await update.message.reply_text("سلام! من آنلاین هستم.")
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("فعلاً فقط دستور /start و /help پشتیبانی می‌شود.")
+def main():
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=BOT_TOKEN,
+        webhook_url=f"{WEBHOOK_URL}/{BOT_TOKEN}",
+    )
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
-
-    print("ربات راه‌اندازی شد...")
-    app.run_polling()
+    main()
